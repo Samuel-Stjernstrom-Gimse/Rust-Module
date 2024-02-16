d63cb6166d3834d45f11df1602d008e586acda43
Zam <samuel.kodehode@gmail.com>
2024-02-16 15:58:28 +0100

    1.0.0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+
+    let x: i8 = 9;
+
+    let y: i8 = 3;
+
+    let z: i8 = x + y;
+
+
+
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).expect("failed to read line");
+
+    println!("{}", input);
+
+
+
+
+
+
 }
